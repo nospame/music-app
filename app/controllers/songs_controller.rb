09f3182032ed734f37b81364628573b201f5797d
nospame <mapson.evan@gmail.com>
2022-03-14 10:32:25 -0700

    add songs controller, routes for index/show/create/update/destroy

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    song.save
+    render json: Song.last.as_json
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.save
+    render json: Song.find_by(id: params[:id]).as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.destroy
+    render json: {message: "Song deleted."}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/songs" => "songs#index"
+  get "/songs/:id" => "songs#show"
+  post "/songs" => "songs#create"
+  patch "/songs/:id" => "songs#update"
+  delete "/songs/:id" => "songs#destroy"
 end
